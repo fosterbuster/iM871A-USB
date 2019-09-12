9c7a6e8dffcf1f889a8a3cc3ee96869c6d1b90c6
Poul Erik Venø <pevh@insero.com>
2019-09-12 17:41:24 +0200

    Setting up basics

---
+++ Example/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    internal class Program
+    {
+        internal static async Task Main( string[] args )
+        {
+            
+        }
+    }
+}
