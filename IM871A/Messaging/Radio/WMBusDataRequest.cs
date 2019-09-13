1d891f884a0fa5fa899ff9efd7f9b081c0ac480d
Poul Erik Venø <pevh@insero.com>
2019-09-13 22:57:05 +0200

    Finished implementing messages, for stuff making sense without having the device.

---
+++ IM871A/Messaging/Radio/WMBusDataRequest.cs
@@ -0,0 +1,36 @@
+﻿// <copyright file="WMBusDataRequest.cs" company="Poul Erik Venø Hansen">
+// Copyright (c) Poul Erik Venø Hansen. All rights reserved.
+// Licensed under the GNU Affero General Public License v3.0 license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+using System.Collections.Generic;
+using IM871A.Utilities.Extensions;
+
+namespace IM871A.Messaging.Radio
+{
+    /// <summary>
+    /// Request.
+    /// This message can be used to send data as an M-Bus message via radio link.
+    /// The first octet of the HCI payload is expected to be the CI- Field of the M-Bus message.The M-Bus Header Fields (C-Field, M-Field and A-Field) are taken from the configuration memory and can be modified via Device Configuration.
+    /// The CRC16 of each M-Bus block and the M-Bus Length Field will be calculated and inserted by the firmware itself.
+    /// </summary>
+    public class WMBusDataRequest : RadioLinkMessage, ITransmittable
+    {
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WMBusDataRequest"/> class.
+        /// </summary>
+        /// <param name="payload">WM-Bus message, starting with C Field.</param>
+        public WMBusDataRequest(byte[] payload)
+            : base(RadioLinkMessageIdentifier.MessageDataRequest, new List<byte>())
+        {
+            Payload.AddRange(payload);
+        }
+
+        /// <inheritdoc/>
+        public override string ToString()
+        {
+            return "WMBus Data Request.";
+        }
+    }
+}
---
+++ IM871A/Messaging/Radio/WMBusMessageIndication.cs
@@ -0,0 +1,30 @@
+﻿// <copyright file="WMBusMessageIndication.cs" company="Poul Erik Venø Hansen">
+// Copyright (c) Poul Erik Venø Hansen. All rights reserved.
+// Licensed under the GNU Affero General Public License v3.0 license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace IM871A.Messaging.Radio
+{
+    /// <summary>
+    /// Indication.
+    /// Whenever the module receives an M-Bus message over the radio link, this message will be passed to the Host Controller.
+    /// The included CRC Fields of the M-Bus message will be removed automatically and only the M-Bus Header and Data Blocks will be transmitted.
+    /// </summary>
+    public class WMBusMessageIndication : RadioLinkMessage, IReceivable
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WMBusMessageIndication"/> class.
+        /// </summary>
+        /// <param name="payload">the payload.</param>
+        public WMBusMessageIndication(byte[] payload)
+            : base(RadioLinkMessageIdentifier.WMBusMessageIndication, payload)
+        {
+        }
+
+        /// <inheritdoc/>
+        public override string ToString()
+        {
+            return "WMBusMessageIndication Response.";
+        }
+    }
+}
